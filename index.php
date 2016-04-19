d5f020f7cb35b83a91c8a2f9a3d382de15861f23
Kevin Marzin <Kevin Marzin>
2016-04-19 17:53:33 +0200

    Script terminé avec bugs (Insertion allemagne + insertion cotes)

--- index.php
+++ index.php
@@ -13,10 +13,9 @@
             catch (Exception $e) {
                 echo "Erreur connexion BDD : " . $e->getMessage();
             }
-            //$result=$pdo->query($query);
             
             /// SUPPRESSION DES TABLES ///
-            /*try {
+            try {
                 $delete = "DELETE FROM ";
 
                 $pdo->exec($delete . "Championnat;");
@@ -31,7 +30,7 @@
             }
             catch (PDOException $e) {
                 echo "Erreur suppression des données : " . $e->getMessage() . '<br>';
-            }*/
+            }
             
             /// REMPLISSAGE DES TABLES ///
             try {
@@ -48,10 +47,14 @@
                                                             . "Renc_Corners_Domicile, Renc_Cartons_Jaune_Exterieur, Renc_Cartons_Jaune_Domicile, "
                                                             . "Renc_Cartons_Rouge_Exterieur, Renc_Cartons_Rouge_Domicile) "
                                                             . "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+                $statement_cote = "INSERT INTO Cote (Cote_Renc_Id, Cote_Bookmaker_Id, Cote_Domicile, Cote_Nul, Cote_Exterieur) "
+                                    . "VALUES (?, ?, ?, ?, ?);";
+                $statement_select_id_club = "SELECT Club_Id FROM Club WHERE Club_Nom = ";
                 
                 
                 $pays = array("England", "France", "Germany", "Italy", "Spain");
                 $champ = array("Premier League", "Ligue 1", "Bundesliga", "Serie A", "Liga");
+                $bookmakers = array("B365", "BW", "IW", "LB", "PS", "WH", "VC");
                 $saisons_id = array();
                 
                 $query_ajout_saison = $pdo->prepare($statement_saison);
@@ -60,9 +63,11 @@
                     array_push($saisons_id, $pdo->lastInsertId());
                 }
                 
-                $query_ajout_pays = $pdo->prepare($statement_pays);
-                $query_ajout_pays->execute(array($p));
-                $id_pays = $pdo->lastInsertId();
+                $query_ajout_bookmaker = $pdo->prepare($statement_bookmaker);
+                for ($bookmaker = 0; $bookmaker < 7; $bookmaker++) {
+                    $query_ajout_bookmaker->execute(array($bookmakers[$bookmaker]));
+                }
+                
                 $i = 0;
                 
                 foreach ($pays as $p) {
@@ -75,52 +80,202 @@
                     $id_champ_pays = $pdo->lastInsertId();
                     $i++;
                     
-                    $dossier = "src/" . $p;
+                    $dossier = "src/" . $p . "/";
                     for ($j = 0; $j <= 9; $j++) {
+                        $row = 1;
                         if (($handle = fopen($dossier . $j . ".csv", "r")) !== FALSE) {
                             while (($data = fgetcsv($handle, ",")) !== FALSE) {
+                                if ($row > 1) {
+                                    $query_club = $pdo->prepare($statement_club);
+                                    if ($query_club->execute(array($data[2], $id_champ_pays))) {
+                                        $home_team_id = $pdo->lastInsertId();
+                                    }
+                                    else {
+                                        $res = $pdo->query($statement_select_id_club . "'". $data[2] ."'");
+                                        $res->setFetchMode(PDO::FETCH_OBJ);
+                                        while( $r = $res->fetch() )
+                                        {
+                                                $home_team_id = $r->Club_Id;
+                                        }
+                                        $res->closeCursor();
+                                    }
+                                    if ($query_club->execute(array($data[3], $id_champ_pays))) {
+                                        $away_team_id = $pdo->lastInsertId();                                        
+                                    }
+                                    else {
+                                        $res = $pdo->query($statement_select_id_club . "'". $data[3] ."'");
+                                        $res->setFetchMode(PDO::FETCH_OBJ);
+                                        while( $r = $res->fetch() )
+                                        {
+                                                $away_team_id = $r->Club_Id;
+                                        }
+                                        $res->closeCursor();
+                                    }
+                                    $query_ajout_rencontre = $pdo->prepare($statement_rencontre);
+                                    $query_ajout_cote = $pdo->prepare($statement_cote);
+                                    if ($p === 'England' || ($p === 'Italy' && ($j === 0 || $j === 1))) {
+                                        $query_ajout_rencontre->execute(array($home_team_id, $away_team_id, $saisons_id[$j], 
+                                                                            $data[1], $data[5], $data[4], $data[12], $data[11],
+                                                                            $data[14], $data[13], $data[16], $data[15], 
+                                                                            $data[18], $data[17], $data[20], $data[19],
+                                                                            $data[22], $data[21]));
+                                        $id_rencontre = $pdo->lastInsertId();
+                                        for ($it = 0; $it < 7; $it++) {
+                                            switch ($data[23 + $i*3]) {
+                                                case "B365H":
+                                                    $bookmaker_id = 1;
+                                                    break;
+                                                case "BWH":
+                                                    $bookmaker_id = 2;
+                                                    break;
+                                                case "IWH":
+                                                    $bookmaker_id = 3;
+                                                    break;
+                                                case "LBH":
+                                                    $bookmaker_id = 4;
+                                                    break;
+                                                case "PSH":
+                                                    $bookmaker_id = 5;
+                                                    break;
+                                                case "WHH":
+                                                    $bookmaker_id = 6;
+                                                    break;
+                                                case "VCH":
+                                                    $bookmaker_id = 7;
+                                                    break;
+                                            }
+                                            $cote_dom = $data[23 + $i*3];
+                                            $cote_nul = $data[23 + $i*3 + 1];
+                                            $cote_ext = $data[23 + $i*3 + 2];
+                                            $query_ajout_cote->execute(array($id_rencontre, $bookmaker_id, $cote_dom, $cote_nul, $cote_ext));
+                                        }
+                                    }
+                                    else if ($p === 'France' && ($j === 0 || $j === 1)) {
+                                        if ($j === 0) {
+                                            $query_ajout_rencontre->execute(array($home_team_id, $away_team_id, $saisons_id[$j], 
+                                                                            $data[1], $data[5], $data[4], $data[11], $data[10],
+                                                                            $data[13], $data[12], NULL, NULL, 
+                                                                            NULL, NULL, $data[15], $data[14],
+                                                                            $data[17], $data[16]));
+                                            $id_rencontre = $pdo->lastInsertId();
+                                            for ($it = 0; $it < 7; $it++) {
+                                                switch ($data[18 + $i*3]) {
+                                                    case "B365H":
+                                                        $bookmaker_id = 1;
+                                                        break;
+                                                    case "BWH":
+                                                        $bookmaker_id = 2;
+                                                        break;
+                                                    case "IWH":
+                                                        $bookmaker_id = 3;
+                                                        break;
+                                                    case "LBH":
+                                                        $bookmaker_id = 4;
+                                                        break;
+                                                    case "PSH":
+                                                        $bookmaker_id = 5;
+                                                        break;
+                                                    case "WHH":
+                                                        $bookmaker_id = 6;
+                                                        break;
+                                                    case "VCH":
+                                                        $bookmaker_id = 7;
+                                                        break;
+                                                }
+                                                $cote_dom = $data[18 + $i*3];
+                                                $cote_nul = $data[18 + $i*3 + 1];
+                                                $cote_ext = $data[18 + $i*3 + 2];
+                                                $query_ajout_cote->execute(array($id_rencontre, $bookmaker_id, $cote_dom, $cote_nul, $cote_ext));
+                                            }
+                                        }
+                                        else {
+                                            $query_ajout_rencontre->execute(array($home_team_id, $away_team_id, $saisons_id[$j], 
+                                                                            $data[1], $data[5], $data[4], $data[11], $data[10],
+                                                                            $data[13], $data[12], NULL, NULL, 
+                                                                            $data[15], $data[14], $data[17], $data[16],
+                                                                            $data[19], $data[18]));
+                                            $id_rencontre = $pdo->lastInsertId();
+                                            for ($it = 0; $it < 7; $it++) {
+                                                switch ($data[20 + $i*3]) {
+                                                    case "B365H":
+                                                        $bookmaker_id = 1;
+                                                        break;
+                                                    case "BWH":
+                                                        $bookmaker_id = 2;
+                                                        break;
+                                                    case "IWH":
+                                                        $bookmaker_id = 3;
+                                                        break;
+                                                    case "LBH":
+                                                        $bookmaker_id = 4;
+                                                        break;
+                                                    case "PSH":
+                                                        $bookmaker_id = 5;
+                                                        break;
+                                                    case "WHH":
+                                                        $bookmaker_id = 6;
+                                                        break;
+                                                    case "VCH":
+                                                        $bookmaker_id = 7;
+                                                        break;
+                                                }
+                                                $cote_dom = $data[20 + $i*3];
+                                                $cote_nul = $data[20 + $i*3 + 1];
+                                                $cote_ext = $data[20 + $i*3 + 2];
+                                                $query_ajout_cote->execute(array($id_rencontre, $bookmaker_id, $cote_dom, $cote_nul, $cote_ext));
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        $query_ajout_rencontre->execute(array($home_team_id, $away_team_id, $saisons_id[$j], 
+                                                                            $data[1], $data[5], $data[4], $data[11], $data[10],
+                                                                            $data[13], $data[12], $data[15], $data[14], 
+                                                                            $data[17], $data[16], $data[19], $data[18],
+                                                                            $data[21], $data[20]));
+                                        $id_rencontre = $pdo->lastInsertId();
+                                        for ($it = 0; $it < 7; $it++) {
+                                            switch ($data[22 + $i*3]) {
+                                                case "B365H":
+                                                    $bookmaker_id = 1;
+                                                    break;
+                                                case "BWH":
+                                                    $bookmaker_id = 2;
+                                                    break;
+                                                case "IWH":
+                                                    $bookmaker_id = 3;
+                                                    break;
+                                                case "LBH":
+                                                    $bookmaker_id = 4;
+                                                    break;
+                                                case "PSH":
+                                                    $bookmaker_id = 5;
+                                                    break;
+                                                case "WHH":
+                                                    $bookmaker_id = 6;
+                                                    break;
+                                                case "VCH":
+                                                    $bookmaker_id = 7;
+                                                    break;
+                                            }
+                                            $cote_dom = $data[22 + $i*3];
+                                            $cote_nul = $data[22 + $i*3 + 1];
+                                            $cote_ext = $data[22 + $i*3 + 2];
+                                            $query_ajout_cote->execute(array($id_rencontre, $bookmaker_id, $cote_dom, $cote_nul, $cote_ext));
+                                        }
+                                    }
+                                    echo "OK POUR RENCONTRE ENTRE " . $data[2] . " ET " . $data[3] . ".<br>";
+                                }
                                 $row++;
-                                $query_club = $pdo->prepare($statement_club);
-                                $query_club->execute($data[2], $england_champ_id);
-                                $home_team_id = $pdo->lastInsertId();
-                                $query_club->execute($data[3], $england_champ_id);
-                                $away_team_id = $pdo->lastInsertId();
-                                $query_ajout_rencontre = $pdo->prepare($statement_rencontre);
-                                if ($p === 'England' || ($p === 'Italy' && ($j = 0 || $j = 1))) {
-                                    $query_ajout_rencontre->execute(array($home_team_id, $away_team_id, $saisons_id[$j], 
-                                                                        $data[1], $data[5], $data[4], $data[12], $data[11],
-                                                                        $data[14], $data[13], $data[16], $data[15], 
-                                                                        $data[18], $data[17], $data[20], $data[19],
-                                                                        $data[22], $data[21]));
-                                }
-                                else if ($p === 'France' && ($j = 0 || $j = 1)) {
-                                    // les deux premieres saisons --> décalage
-                                }
-                                else {
-                                    $query_ajout_rencontre->execute(array($home_team_id, $away_team_id, $saisons_id[$j], 
-                                                                        $data[1], $data[5], $data[4], $data[11], $data[10],
-                                                                        $data[13], $data[12], $data[15], $data[14], 
-                                                                        $data[17], $data[16], $data[19], $data[18],
-                                                                        $data[21], $data[20]));
-                                }
-                                $id_rencontre = $pdo->lastInsertId();
-                                // Insérer les côtes
-                                
                             }
                             fclose($handle);
                         }
                     }
                 }
+                
             }
             catch (PDOException $e) {
                 echo $e->getMessage();
             }
-            
-            /// SELECT ///
-            /*$res = $pdo->query();
-            while ($donnees = $res->fetch()) {
-                echo $donnees['NOM_CHAMP'];
-            }*/
             
             $pdo=null;
         }
